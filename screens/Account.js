73b3f75681c2d63ddfc075cbf75c3899b9184688
Hakan <celikhakan5255@gmail.com>
2022-03-12 15:52:14 +0300

    Firebase Complete

--- screens/Account.js
+++ screens/Account.js
@@ -1,11 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, useColorScheme, TouchableOpacity} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {COLORS} from '../Colors';
 import {setData} from '../redux/slices/UserSlices';
 
 import {StyledTitle} from '../components/main/StyledComponents';
+
+import firebase from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
 
 const Account = ({navigation}) => {
   const colorSchema = useColorScheme();
@@ -17,6 +20,13 @@ const Account = ({navigation}) => {
     dispatch(setData(''));
     navigation.navigate('Anasayfa');
   };
+
+  useEffect(() => {
+    messaging()
+      .getToken(firebase.app().options.messagingSenderId)
+      .then(x => console.log(x))
+      .catch(e => console.log(e));
+  }, []);
 
   return (
     <View
